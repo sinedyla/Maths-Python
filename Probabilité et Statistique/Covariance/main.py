2d0bd06fa6fcb0a48e470d06b241aa27972c26b0
sinedyla <sinedyla@gmail.com>
2025-10-24 23:28:16 +0000

    Nouveau programme

--- Probabilité et Statistique/Covariance/main.py
+++ Probabilité et Statistique/Covariance/main.py
@@ -1,2 +1,14 @@
 from numpy import *
 
+X = [1,2,3]
+Y = [4,5,6]
+
+P = [0.1,0.2,0.3]
+
+Ex = sum(x*p for x,p in zip(X,P))
+
+Ey = sum(y*p for y,p in zip(Y,P))
+
+covariance = sum((x-Ex)*(y-Ey)*p for x,y,p in zip(X,Y,P))
+
+print(covariance)
