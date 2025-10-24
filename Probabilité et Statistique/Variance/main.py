762e65da29bd066b1867fdb01ecef678a2858120
sinedyla <sinedyla@gmail.com>
2025-10-24 23:15:02 +0000

    Nouveau programme

--- Probabilité et Statistique/Variance/main.py
+++ Probabilité et Statistique/Variance/main.py
@@ -1,12 +1,10 @@
 from numpy import *
 
 X = [1,2,3]
-Px = [0.2, 0.3, 0.5]
+Y = [4,5,6]
 
 esperance = sum(x*p for x,p in zip(X,Px))
 
 esperance_carre = sum(x**2 * p for x,p in zip(X,Px))
 
 variance = esperance_carre - (esperance)**2
-
-print(variance)
